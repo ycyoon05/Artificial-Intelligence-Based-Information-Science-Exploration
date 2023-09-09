0fc7ebfda510a2081e3424018260afa2a15df8cd
Noeul <ycyoon0522@gmail.com>
2023-09-09 11:19:42 +0900

    week03 chap02 v0.2

--- week03_chap02.py
+++ week03_chap02.py
@@ -1,12 +1,7 @@
-student_id = 20115
-student_name = "윤영찬"
-subject = ["파이썬", "통계학", "수학"]
-score = 3.7
+age = int(input("나이는 : "))
+print(age >= 19)
 
-print(f"학번이 {student_id}인 {student_name} 학생은 {subject}을(를) 수강하였고, 학점이 {score}입니다.")
-
-print(type(student_id), type(student_name), type(subject), type(score), type(True)) # 각 변수의 자료 타입 출력
-print(id(student_id), id(student_name), id(subject), id(score)) # 각 변수의 ID값 출력
-
-if False: # boolean type
-    print("무조건 출력 안됩니다.")
+if age >= 19:
+    print("극장 입장이 가능합니다.")
+else:
+    print("미성년자는 입장이 불가합니다.")
