420343263eed3637e1fb489e4a1e93d6ba84eb00
Noeul <ycyoon0522@gmail.com>
2023-09-09 11:51:21 +0900

    week03 chap2 v0.4

--- week03_chap02.py
+++ week03_chap02.py
@@ -1,17 +1,5 @@
-subjects = ["파이썬", "음악", "체육"]
-print(subjects)
-subjects[1] = "통계학" # mutable
-print(subjects)
-
-# True = False # immutable
-
-_teacher = "김과학"
-print(_teacher)
-_teacher = "박수학"
-print(_teacher)
-
-# 99 = 7
-# "kim" = "park"
-score = 100
-# if = 9.7
-# False = 8
+# a = 7
+a = '7'
+b = a + '99'
+print(b)
+print(int(a) + int('99'))
