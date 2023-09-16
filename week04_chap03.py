203d09b299f59867fe08d0091afdc0ccc9799c53
Noeul <ycyoon0522@gmail.com>
2023-09-16 10:24:01 +0900

    week04 chap03 v0.4

--- week04_chap03.py
+++ week04_chap03.py
@@ -5,3 +5,11 @@ b = 0b1010110001110001
 c = 0o126161
 d = 44145
 
+print(a, b, c, d)
+print(hex(d), bin(d), oct(d), d)
+print(chr(66)) # A ~ Z (65 ~ 90)
+print(ord('B'))
+print(chr(98)) # a ~ z (97 ~ 122)
+print(ord('김'))
+print(chr(0xAE40))
+print(chr(44608))
