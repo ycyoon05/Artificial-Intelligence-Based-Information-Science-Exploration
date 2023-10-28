f87d64f38768268afc1011ba89476e6371a40867
Noeul_ <ycyoon0522@gmail.com>
2023-10-28 10:44:09 +0900

    week07 chap07 v0.2

--- week07_chap07.py
+++ week07_chap07.py
@@ -4,15 +4,21 @@ for i in range(0, 11):
     numbers.append(i)
 
 print(numbers)
-print(numbers[-2])
-print(numbers[-11], numbers[0])
-
-odd_numbers = numbers[1::2]
-even_numbers = numbers[0::2]
-reverse_odd_numbers = numbers[-2::-2]
 reverse_numbers = numbers[::-1]
-
-print(odd_numbers)
-print(even_numbers)
-print(reverse_odd_numbers)
 print(reverse_numbers)
+print(numbers)
+numbers.reverse()
+print(numbers)
+
+numbers.append(-11)
+print(numbers)
+
+numbers.insert(5, 99)
+print(numbers)
+numbers.insert(-2, 55)
+print(numbers)
+
+numbers *= 2 # list multiplication
+print(numbers)
+
+
