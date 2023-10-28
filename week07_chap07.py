b4dd3c5368efd5a7348ffab5a3e9acdef30d354b
Noeul_ <ycyoon0522@gmail.com>
2023-10-28 11:55:59 +0900

    week07 chap07 v0.5

--- week07_chap07.py
+++ week07_chap07.py
@@ -1,45 +1,8 @@
-trolly = ['김광석CD', '아이폰15프로']
-print(trolly)
-
-foods = ['다시마', '멸치']
-trolly.append(foods)
-print(trolly)
-print(trolly[2][1])
-# print(trolly.pop()) # 리스트의 맨 마지막 원소(['다시마', '멸치'])를 리턴 후 삭제
-# print(trolly[2].pop()) # 멸치 원소를 리턴 후 삭제
-print(trolly[2].pop(0)) # 다시마 원소를 리턴 후 삭제
-print(trolly)
-
-trolly[2].append('다시마')
-print(trolly)
-
-trolly[2].insert(0, '딸기')
-print(trolly)
-
-# trolly.pop(1)
-# trolly.insert(1, '아이폰15')
-# print(trolly)
-
-# trolly[1:2] = '아이폰15' # ['김광석CD', '아', '이', '폰', '1', '5', ['딸기', '멸치', '다시마']]
-# trolly[1:2] = ['아이폰15']
-trolly[1:2] = ('아이폰15',) # tuple
-print(trolly)
-
-# 3가지 삭제 방법
-# trolly.pop(0)
-# trolly.remove('김광석CD')
-del trolly[0]
-print(trolly)
-
-# trolly.clear() # 드롤리 원소 모두 삭제
-print(trolly.index('아이폰15'))
-print(trolly)
-
-print('멸치' in trolly)
-print('아이폰15' in trolly)
-print('멸치' in trolly[1])
-print(trolly.count('감자'))
-print(trolly.count('아이폰15'))
-trolly.append('아이폰15')
-print(trolly)
-print(trolly.count('아이폰15'))
+fruits = ['딸기', '망고', '포도', '사과', '골드키위']
+print(fruits)
+fruits_string = ' '.join(fruits) # 리스트의 원소들을 문자열로 변환
+print(fruits_string)
+school = 'Incheon Science High School'
+# school_list = school.split() # 문자열을 리스트로 변환, 구분자 기본 값은 띄어쓰기
+school_list = school.split('S') # 문자열을 리스트로 변환, 구분자 기본 값은 띄어쓰기
+print(school_list)
