3df5731f4d43b87bdbbcfe5e7280549a7ba6292f
Noeul_ <ycyoon0522@gmail.com>
2023-10-28 11:47:43 +0900

    week07 chap07 v0.5

--- week07_chap07.py
+++ week07_chap07.py
@@ -21,5 +21,25 @@ print(trolly)
 # print(trolly)
 
 # trolly[1:2] = '아이폰15' # ['김광석CD', '아', '이', '폰', '1', '5', ['딸기', '멸치', '다시마']]
-trolly[1:2] = ['아이폰15']
+# trolly[1:2] = ['아이폰15']
+trolly[1:2] = ('아이폰15',) # tuple
 print(trolly)
+
+# 3가지 삭제 방법
+# trolly.pop(0)
+# trolly.remove('김광석CD')
+del trolly[0]
+print(trolly)
+
+# trolly.clear() # 드롤리 원소 모두 삭제
+print(trolly.index('아이폰15'))
+print(trolly)
+
+print('멸치' in trolly)
+print('아이폰15' in trolly)
+print('멸치' in trolly[1])
+print(trolly.count('감자'))
+print(trolly.count('아이폰15'))
+trolly.append('아이폰15')
+print(trolly)
+print(trolly.count('아이폰15'))
